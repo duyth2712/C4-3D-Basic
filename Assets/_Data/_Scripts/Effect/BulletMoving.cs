efa561d28ea9848e689a27d8791776ff9af07894
Phuong <duyth1227@gmmail.com>
2024-10-25 16:23:50 +0700

    d4

---
+++ Assets/_Data/_Scripts/Effect/BulletMoving.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMoving : TempBehavior
+{
+    public float speed = 10f;
+    protected virtual void Update()
+    {
+        this.Moving();
+    }
+
+    protected virtual void Moving()
+    {
+        transform.parent.Translate(Vector3.forward * this.speed * Time.deltaTime);
+    }
+}
