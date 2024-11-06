645a3711a8078fd82f9fe20e1e2b2826b511e462
Phuong <duyth1227@gmmail.com>
2024-11-07 03:20:25 +0700

    week05

--- Assets/_Data/_Scripts/Effect/EffectCtrl.cs
+++ Assets/_Data/_Scripts/Effect/EffectCtrl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EffectSpawner : Spawner<EffectCtrl>
+public abstract class EffectCtrl : PoolObj
 {
     
 }
---
+++ Assets/_Data/_Scripts/Spawner/Despawn/DespawnBase.cs
@@ -0,0 +1,5 @@
+
+public abstract class DespawnBase : TempBehavior
+{
+    public abstract void DoDespawn();
+}
